bf6c793b28d9df6f1a3c8a10e63876cd96a80b9d
Bruno Cochofel <bruno.cochofel@gmail.com>
2020-03-27 15:33:35 +0000

    prepare repo for terraform cloud

--- k8s.tf
+++ k8s.tf
@@ -7,40 +7,11 @@ resource "random_id" "salt" {
   byte_length = 4
 }
 
-resource "azurerm_log_analytics_workspace" "test" {
-  # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-  name                = "${var.log_analytics_workspace_name}-${random_id.salt.dec}"
-  location            = var.log_analytics_workspace_location
-  resource_group_name = azurerm_resource_group.k8s.name
-  sku                 = var.log_analytics_workspace_sku
-}
-
-resource "azurerm_log_analytics_solution" "test" {
-  solution_name         = "ContainerInsights"
-  location              = azurerm_log_analytics_workspace.test.location
-  resource_group_name   = azurerm_resource_group.k8s.name
-  workspace_resource_id = azurerm_log_analytics_workspace.test.id
-  workspace_name        = azurerm_log_analytics_workspace.test.name
-
-  plan {
-    publisher = "Microsoft"
-    product   = "OMSGallery/ContainerInsights"
-  }
-}
-
 resource "azurerm_kubernetes_cluster" "k8s" {
   name                = var.cluster_name
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = var.dns_prefix
-
-  linux_profile {
-    admin_username = "ubuntu"
-
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
-  }
 
   default_node_pool {
     name       = "agentpool"
@@ -53,15 +24,8 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     client_secret = var.client_secret
   }
 
-  addon_profile {
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
-    }
-  }
-
   tags = {
-    Environment = "Development"
+    environment = "lab"
   }
 }
 
